88b19c710f3a7dc9d9151940364262442a5d568d
rounittxx <raunitsingh@Raunits-MacBook-Pro-2.local>
2026-01-16 00:50:11 +0530

    changes

--- app.py
+++ app.py
@@ -130,4 +130,9 @@ webrtc_streamer(
     video_processor_factory=GestureCanvas,
     media_stream_constraints={"video": True, "audio": False},
     async_processing=True,
+    rtc_configuration={
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]}
+        ]
+    }
 )
